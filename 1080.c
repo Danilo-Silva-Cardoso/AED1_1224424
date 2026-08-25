c1387c0eecbd090766dab643e2ce810439bd61f8
Danilo-Silva-Cardoso <danilo.cardoso@unifesp.br>
2026-08-24 21:32:22 -0300

    Create 1080.c

---
+++ 1080.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main() {
+    int valor, maior = 0, posicao = 0;
+
+    for (int i = 1; i <= 100; i++) {
+        scanf("%d", &valor);
+
+        if (valor > maior) {
+            maior = valor;
+            posicao = i;
+        }
+    }
+
+    printf("%d\n%d\n", maior, posicao);
+
+    return 0;
+}
