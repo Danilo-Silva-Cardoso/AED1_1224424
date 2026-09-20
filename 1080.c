fd3cefd99e6feb4773b6dae7101d4c50bb674daa
Danilo-Silva-Cardoso <danilo.cardoso@unifesp.br>
2026-09-20 15:37:54 -0300

    Update 1080.c

--- 1080.c
+++ 1080.c
@@ -1,3 +1,14 @@
+/* --------------------------------------------------------------------------
+Disciplina  : Algortimo e Estrutura de Dados 2026S1
+Nome        : Danilo Da Silva Cardoso
+Linguagem   : C
+Problema    : https://judge.beecrowd.com/pt/problems/view/1080
+Data        : 27/08/2026
+Objetivo    : Checagem de parenteses
+Dificuldade : sintaxe do c
+Uso de IA   : No maximo onde estava meus erros
+-------------------------------------------------------------------------- */
+
 #include <stdio.h>
 
 int main() {
