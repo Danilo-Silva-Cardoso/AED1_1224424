d71b05449aa6bf297f9cdf6f6ff8d9868e58144a
Danilo-Silva-Cardoso <danilo.cardoso@unifesp.br>
2026-09-20 15:42:26 -0300

    Update 1110.C

--- 1110.C
+++ 1110.C
@@ -1,3 +1,14 @@
+/* --------------------------------------------------------------------------
+Disciplina  : Algortimo e Estrutura de Dados 2026S1
+Nome        : Danilo Da Silva Cardoso
+Linguagem   : C
+Problema    : https://judge.beecrowd.com/pt/problems/view/1110
+Data        : 20/09/2026
+Objetivo    : Numero de descartes e quantas cartas sobraram
+Dificuldade : Fazer a estrutura da lista
+Uso de IA   : Perguntei onde estava errando e fui pedindo atraves de perguntas sem ela me dar a resposta como me ajudar
+-------------------------------------------------------------------------- */
+
 #include <stdio.h>
 #include <stdlib.h>
 
